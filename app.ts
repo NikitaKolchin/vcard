bfc22b01eeacfe01479f6c4e57a9c8fa407b5205
Nikita Kolchin <kolchin.nv@gmail.com>
2023-03-08 21:32:43 +0300

    added ts app

--- app.ts
+++ app.ts
@@ -1,17 +1,30 @@
-const VCard = require('vcard-creator').default
-const fs = require('fs')
-let data =''
-let rawdata = fs.readFileSync('./files/input/ui.json')
-let items = JSON.parse(rawdata)
-const getSimplePhones = (rawPhonesString) => rawPhonesString?.split(";").map(phohe => phohe.trim())
-const getDialedPhones  = (rawPhonesString) => getSimplePhones(rawPhonesString)?.map(phone => {
+import VCard from 'vcard-creator';
+import * as fs from 'fs';
+let data: string ='';
+
+interface Card {
+    Title : string,
+    Position : string,
+    Email: string,
+    WorkPhone: string,
+    AdditionPhone: string,
+    Department: Department
+}
+
+interface Department {
+    Title:string
+}
+let rawdata = fs.readFileSync('./files/input/ui.json').toString("utf-8")
+let items: Card[] = JSON.parse(rawdata)
+const getSimplePhones = (rawPhonesString: string):string[] => rawPhonesString?.split(";").map((phohe: string) => phohe.trim())
+const getDialedPhones  = (rawPhonesString: string):string[] => getSimplePhones(rawPhonesString)?.map((phone: string) => {
   if (phone.startsWith('(831) 431-9'))
     return phone.replace('(831) 431-9', '243-1')
   if (phone.startsWith('(831) 249') || '(831) 438')
     return phone.replace(/.{9}/, '243')
   return phone
 })
-const handleItems = (element) => {
+const handleItems = (element: Card) => {
   const myVCard = new VCard()
   myVCard
   .addName(element.Title)
